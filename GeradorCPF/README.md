c043fb8288fbfb01e5f66346f831ef4deaeca67d
Andre Parron <andreparoon@gmail.com>
2023-12-11 17:48:00 -0300

    Primeiro Exercício Com Tecnologias Mais Avançadas

---
+++ GeradorCPF/README.md
@@ -0,0 +1,3 @@
+Um gerador simples de CPF apenas para praticar utilização de Import, Export, Classes e Módulos no geral.
+Está sendo utilizado também WebPack e associado à ele, Babel para criação de um código JavaScript compatível com todos os navegadores antigos.
+Além também de uma nova organização de arquivos.
---
+++ GeradorCPF/src/modules/ValidaCPF.js
@@ -0,0 +1,49 @@
+export default class ValidaCPF {
+  //Método Construtor
+  constructor(cpfEnviado) {
+    Object.defineProperty(this, "cpfLimpo", {
+      enumerable: true,
+      get: function () {
+        return cpfEnviado.replace(/\D+/g, "");
+      },
+    });
+  }
+  //Funções e Métodos
+  //Funçao valida
+  valida() {
+    if (typeof this.cpfLimpo === "undefined") return false;
+    if (this.cpfLimpo.length !== 11) return false;
+    if (this.isSequencia()) return false;
+
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = ValidaCPF.criaDigito(cpfParcial);
+    const digito2 = ValidaCPF.criaDigito(cpfParcial + digito1);
+
+    const novoCPF = cpfParcial + digito1 + digito2;
+
+    return novoCPF === this.cpfLimpo;
+  }
+  //Funçao que cria os 2 digitos finais do CPF
+  //Como não possui nenhuma utilização de this dentro dessa função
+  //A função foi transformada em função estática apenas para fins de treinamento.
+  static criaDigito(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+      ac += regressivo * Number(val);
+      regressivo--;
+      return ac;
+    }, 0);
+
+    const digito = 11 - (total % 11);
+
+    return digito > 9 ? "0" : String(digito);
+  }
+
+  //Função para apenas conferir se o CPF não é uma sequencia de numeros iguais.
+  isSequencia() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+  };
+}
